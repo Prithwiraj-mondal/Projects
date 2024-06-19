88f0eaca4b3c294327da6f4e9c7d6221198d3cca
Prithwiraj-mondal <mondalprithwiraj009@gmail.com>
2024-06-19 22:05:51 +0530

    Create if-else(D-1).py

---
+++ if-else(D-1).py
@@ -0,0 +1,21 @@
+# a = "words"
+# b = 10
+# c = 10.25
+# d = True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+                #<class 'str'>
+                # <class 'int'>
+                # <class 'float'>
+                # <class 'bool'>
+k=["guava","mango","banana","lichi","papaya"]
+#  0,1,2 ,3
+# -3,-2,-1,0
+u=[9,4,10,85]
+print(k)
+print(len(k))
+print("-",len(u)-1)
+print(u[0:4])
+print(u[-3])
